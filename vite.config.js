10df4db647d4c379283f6053ace4044adf4a7edb
shikeik <2667097598@qq.com>
2026-03-30 12:13:06 +0800

    修复vite路径别名，添加@ai和@views别名

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,9 @@ export default {
   resolve: {
     alias: {
       '@game': resolve(__dirname, 'src/game'),
-      '@render': resolve(__dirname, 'src/render')
+      '@render': resolve(__dirname, 'src/render'),
+      '@ai': resolve(__dirname, 'src/ai'),
+      '@views': resolve(__dirname, 'src/views')
     }
   }
 }
